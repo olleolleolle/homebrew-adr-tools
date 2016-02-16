f20171cd7bb737b888befe6d8296bd28eca77be0
Olle Jonsson <olle.jonsson@ecraft.com>
2016-02-16 14:51:05 +0100

    inreplac

--- adr_tools.rb
+++ adr_tools.rb
@@ -5,6 +5,10 @@ class AdrTools < Formula
   sha256 "ceeba995d2319ba2735dd3c613bf145070efb8984886887a9edde7d498354b4b"
 
   def install
+    inreplace 'src/adr-init', '$(dirname $0)', prefix
+    inreplace 'src/adr-new', 'ADR_TEMPLATE:-$(dirname $0)/template.md', "ADR_TEMPLATE:-#{prefix}/template.md"
+
+    prefix.install Dir["src/*.md"]
     bin.install Dir["src/*"] - Dir["src/*.md"]
   end
 
