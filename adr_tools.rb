2ff9b86c35af5a4cdae30b4e89cb9cd82b01442a
Olle Jonsson <olle.jonsson@ecraft.com>
2016-02-16 11:10:39 +0100

    adr-tools Formula

---
+++ adr_tools.rb
@@ -0,0 +1,14 @@
+class AdrTools < Formula
+  desc "A command-line tool for working with Architecture Decision Records (ADRs)."
+  homepage "https://github.com/npryce/adr-tools"
+  url "https://codeload.github.com/npryce/adr-tools/tar.gz/1.0.0"
+  sha256 "ceeba995d2319ba2735dd3c613bf145070efb8984886887a9edde7d498354b4b"
+
+  def install
+    bin.install Dir["src/*"] - Dir["src/*.md"]
+  end
+
+  test do
+    system "true"
+  end
+end
